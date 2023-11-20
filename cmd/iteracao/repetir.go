12aeff89c9c65f61fc1840dd7b49b236121e912a
fabiocaettano <fabio.caettano74@gmail.com>
2023-11-20 10:14:34 -0300

    feat: implementado for

--- cmd/iteracao/repetir.go
+++ cmd/iteracao/repetir.go
@@ -1,8 +1,10 @@
 package iteracao
 
+const numeroRepeticoes = 5
+
 func Repetir(letra string) string {
 	var repeticoes string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < numeroRepeticoes; i++ {
 		repeticoes = repeticoes + letra
 	}
 	return repeticoes
